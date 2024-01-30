a6948978486b2a341dc27172a9a5e70da4944529
gowthamijtd <148223456+gowthamijtd@users.noreply.github.com>
2024-01-30 14:21:49 +0530

    Add files via upload

---
+++ assignment -method2.py
@@ -0,0 +1,19 @@
+list1 = [
+    { 'firstName': 'Noah', 'lastName': 'M.', 'country': 'Switzerland', 'continent': 'Europe', 'age': 19, 'language': 'C', 'meal': 'vegetarian' },
+    { 'firstName': 'Anna', 'lastName': 'R.', 'country': 'Liechtenstein', 'continent': 'Europe', 'age': 52, 'language': 'JavaScript', 'meal': 'standard' },
+    { 'firstName': 'Ramona', 'lastName': 'R.', 'country': 'Paraguay', 'continent': 'Americas', 'age': 29, 'language': 'Ruby', 'meal': 'vegan' },
+    { 'firstName': 'George', 'lastName': 'B.', 'country': 'England', 'continent': 'Europe', 'age': 81, 'language': 'C', 'meal': 'vegetarian' },
+]
+def food():
+    b=[index['meal'] for index in list1]
+    return b
+a=food()
+def count():
+    vegetarian=("vegetarian:",a.count("vegetarian"))
+    standard=("standard:",a.count("standard"))
+    vegan=("vegan:",a.count("vegan"))
+    return vegetarian,standard,vegan
+c=count()
+food_count=dict(c)
+print(food_count)
+
